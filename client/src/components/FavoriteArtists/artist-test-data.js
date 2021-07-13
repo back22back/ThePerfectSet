3e77aea1922a6759446535eb74451481d61f8e9d
Vy Tran <vtran1022@Gmail.com>
2021-07-13 16:45:26 -0700

    artist testdata

---
+++ client/src/components/FavoriteArtists/artist-test-data.js
@@ -0,0 +1,123 @@
+const artistData = [
+	{
+		name: 'Korn',
+		website: 'https://www.kornofficial.com/thenothing/',
+		bio: 'Korn is an American nu metal band from Bakersfield, California, formed in 1993. The band is notable for pioneering the nu metal genre and bringing it into the mainstream.',
+		tour: [
+			{
+				event: 'Palm Beach, FL',
+				url: 'https://www.bandsintown.com/e/102602334'
+			},
+			{
+				event: 'Cadott, WI',
+				url: 'https://www.bandsintown.com/e/102384587'
+			},
+			{
+				event: 'Rapids, MI',
+				url: 'https://www.bandsintown.com/e/102396907'
+			}
+		]
+	},
+	{
+		name: 'Rammstein',
+		website: 'https://www.rammstein.de/en/',
+		bio: 'Rammstein is a German Neue Deutsche Härte band formed in Berlin in 1994.',
+		tour: [
+			{
+				event: 'Letiště Letňany, PRAHA',
+				url: 'https://www.eventim.cz/event/rammstein-letiste-letnany-13733527'
+			},
+			{
+				event: 'Coventry, UK',
+				url: 'https://www.eventim.co.uk/event/rammstein-europe-stadium-tour-2020-ricoh-arena-12130932'
+			},
+			{
+				event: 'Ullevi, GÖTEBORG',
+				url: 'https://www.ticketmaster.se/event/552955'
+			}
+		]
+	},
+	{
+		name: 'Rage Against the Machine',
+		website: 'https://tour.ratm.com/',
+		bio: 'Rage Against the Machine is an American rock band from Los Angeles, California. Formed in 1991, their songs express revolutionary political views.',
+		tour: [
+			{
+				event: 'El Paso, TX',
+				url: 'https://www.ticketmaster.com/event/0C005779A45444D8'
+			},
+			{
+				event: 'Las Cruces, NM',
+				url: 'https://www.ticketmaster.com/event/0C0057762EAB9E74'
+			},
+			{
+				event: 'Glendale, AZ',
+				url: 'https://www.ticketmaster.com/event/1900577804F53030'
+			}
+		]
+	},
+	{
+		name: 'Metallica',
+		website: 'https://www.metallica.com/',
+		bio: 'Metallica is an American heavy metal band. The band was formed in 1981 in Los Angeles by vocalist/guitarist James Hetfield and drummer Lars Ulrich, and has been based in San Francisco for most of its career.',
+		tour: [
+			{
+				event: 'Louisville, KY',
+				url: 'https://louderthanlife.frontgatetickets.com/'
+			},
+			{
+				event: 'Daytona Beach, FL',
+				url: 'https://rockville.frontgatetickets.com/'
+			},
+			{
+				event: 'Prague, Czech Republic',
+				url: 'https://www.ticketmaster.cz/artist/prague-rocks-vstupenky/1062292'
+			}
+		]
+	},
+	{
+		name: 'Tiësto',
+		website: 'https://www.tiesto.com/',
+		bio: 'Tijs Michiel Verwest OON, known professionally as Tiësto, is a Dutch DJ and record producer from Breda, North Brabant.',
+		tour: [
+			{
+				event: 'Las Vegas, NV',
+				url: 'https://zoukgrouplv.com/event/EVE65055600020210717/ti-sto/'
+			},
+			{
+				event: 'South Padre Island, TX',
+				url: 'https://ljconciertos.com/events/101/tickets'
+			},
+			{
+				event: 'Boston, MA',
+				url: 'https://www.ticketmaster.com/event/01005AE6D2FF3C3C'
+			}
+		]
+	},
+	{
+		name: 'David Guetta',
+		website: 'https://davidguetta.com/',
+		bio: 'Pierre David Guetta is a French DJ, record producer and songwriter. He has racked up over 50 million record sales globally, with more than 10 billion streams.',
+		tour: [
+			{
+				event: 'Andorra Mountain Music',
+				url: 'https://davidguetta.com/event/andorra-mountain-music-andorra-la-vella-ad/'
+			}
+		]
+	},
+	{
+		name: 'NERVO',
+		website: 'http://nervomusic.com/',
+		bio: 'Nervo (stylised as NERVO) are an Australian DJ duo comprising twin sisters Olivia and Miriam Nervo (born 18 February 1982).',
+		tour: [
+			{
+				event: 'Ohrid, North Macedonia',
+				url: 'https://www.bandsintown.com/e/102741305'
+			},
+			{
+				event: 'Bucharest, Romania',
+				url: 'https://www.bandsintown.com/e/1021819259'
+			}
+		]
+	}
+];
