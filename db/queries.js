998bb77c16fc6666c058797774d30f8124a2bcf2
Alexander Causey <alexander.w.causey@gmail.com>
2021-07-13 15:59:22 -0700

    added db query and server endpoint to retrieve a list of artists a user follows

--- db/queries.js
+++ db/queries.js
@@ -11,12 +11,12 @@ const addBooking = (date, type, id, lat, lon) => {
 	return pool.query(queryStr, [date, type, id, lat, lon]).then((response) => response);
 };
 
-const fetchFollows = (fan_id) => {
+const fetchFollows = (user_id) => {
 	const queryStr = `
 	json_agg(
 		SELECT artist_id FROM follows WHERE fan_id=$1
 	)`
-	return pool.query(queryStr, [fan_id]).then((response) => response);
+	return pool.query(queryStr, [user_id]).then((response) => response);
 };
 
 module.exports = {
