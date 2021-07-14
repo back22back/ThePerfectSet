8be849a0ab106e5db516b3dc4782d5d12d4ab46b
Vy Tran <vtran1022@Gmail.com>
2021-07-14 16:44:19 -0700

    added routes comment

--- client/src/components/FaveArtComponent/FavoriteArtists.jsx
+++ client/src/components/FaveArtComponent/FavoriteArtists.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
 import { Container, Button, Accordion, Card, Image, Row } from 'react-bootstrap';
+import { IoMdArrowBack, IoMdCloseCircle } from 'react-icons/Io';
+import { VscSettings } from 'react-icons/Vsc';
+
+//need to import fan home page and fan search page
 
 import artistData from './artist-test-data.js';
 import placeholder from './placeholder.png';
 
 const FavoriteArtists = () => {
-	const [pet, setPet] = useState('dog');
-
 	return (
+		// <Router>
 		<Container>
+			<nav>
+				{/* <Link to='/app' component={App}> */}
+				<IoMdArrowBack />
+				{/* </Link> */}
+				<VscSettings />
+			</nav>
 			<Row>
 				<div className='container-fluid'>
 					<Image src={placeholder} style={{ width: '100%', height: 'auto' }}></Image>
@@ -32,6 +41,7 @@ const FavoriteArtists = () => {
 				</div>
 			</Row>
 		</Container>
+		// </Router>
 	);
 };
 
