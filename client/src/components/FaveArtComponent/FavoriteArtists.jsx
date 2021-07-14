0dfad055b0aa95c503b193fa85859a4cb89cab3f
Vy Tran <vtran1022@Gmail.com>
2021-07-13 21:52:14 -0700

    added accordion for faveartists and placeholder image

--- client/src/components/FaveArtComponent/FavoriteArtists.jsx
+++ client/src/components/FaveArtComponent/FavoriteArtists.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import Artist from './Artist.jsx';
+import { Link } from 'react-router-dom';
+import { Container, Button, Accordion, Card, Image, Row } from 'react-bootstrap';
+
 import artistData from './artist-test-data.js';
+import placeholder from './placeholder.png';
 
 const FavoriteArtists = () => {
 	return (
-		<>
-			Placeholder for album image
-			{artistData.map((artist) => (
-				<span>{artist.name}</span>
-			))}
-		</>
+		<Container>
+			<Row>
+				<Image src={placeholder} style={{ width: '100%', height: 'auto' }}></Image>
+			</Row>
+			<Row>
+				<Accordion>
+					{artistData.map((artist, i) => (
+						<Card key={`${artist}${i * 20}`}>
+							<Accordion.Toggle as={Card.Header} eventKey={i}>
+								{artist.name}
+							</Accordion.Toggle>
+							<Accordion.Collapse eventKey={i}>
+								<Card.Body>{artist.bio}</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+					))}
+				</Accordion>
+			</Row>
+		</Container>
 	);
 };
 
