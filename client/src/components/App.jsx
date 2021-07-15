4dc5fd752bb2c239e89012b74dfdcd6e31400110
Elizabeth Mendenhall <eamendenhall@outlook.com>
2021-07-15 12:00:24 -0700

    moved <Router> to App.jsx. Added themes object.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,6 +7,8 @@ import Register from './registration-login/Register.jsx';
 import RegistrationLogin from './registration-login/RegistrationLogin.jsx';
 import AppContext from './AppContext.js'
 import TestHome from './test/TestHome.jsx'
+import themes from './themes.js';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -23,11 +25,18 @@ const App = () => {
   let [website, setWebsite] = useState('www.efgsdfg.com')
   let [isArtist, setIsArtist] = useState('false')
   let serverUrl = 'serverurl'
-
+  let theme = themes.neon
   return (
     <>
-      <AppContext.Provider value={{username, setUsername, bio, setBio, website, setWebsite, serverUrl, isArtist, setIsArtist}} >
-        <RegistrationLogin/>
+      <AppContext.Provider value={{username, setUsername, bio, setBio, website, setWebsite, serverUrl, isArtist, setIsArtist, theme}} >
+      <Router>
+      <Switch>
+        <Route path="/" exact component={SplashPage} />
+        <Route path="/Login" exact component={Login} />
+        <Route path="/Register" exact component={Register} />
+        <Route path="/TestHome" exact component={TestHome} />
+      </Switch>
+    </Router>
       </AppContext.Provider>
     </>
   )
