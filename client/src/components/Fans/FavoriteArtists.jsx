075364936bdd4a9861d809be6d796a017ec74284
Vy Tran <vtran1022@Gmail.com>
2021-07-15 16:28:13 -0700

    styling down, connected to db

--- client/src/components/Fans/FavoriteArtists.jsx
+++ client/src/components/Fans/FavoriteArtists.jsx
@@ -4,21 +4,19 @@ import { Container, Button, Accordion, Card, Image, Row, Col } from 'react-boots
 import { IoMdArrowBack, IoMdCloseCircle } from 'react-icons/Io';
 import { VscSettings } from 'react-icons/Vsc';
 import axios from 'axios';
-import FanHome from './FanHome.jsx';
-
-import artistData from './artist-test-data.js';
 import fanfave from './fanfave.jpeg';
 
 const FavoriteArtists = () => {
+	const [artistList, setArtistList] = useState([]);
 	const fetchArtists = () => {
 		axios
 			.get('/follows', { params: { user_id: 3 } })
-			.then((data) => console.log('artist information', data.data))
+			.then((data) => setArtistList(data.data))
 			.catch((err) => console.log(`Error fetching favorite artists: ${err}`));
 	};
 
 	useEffect(() => {
-		// fetchArtists();
+		fetchArtists();
 	}, []);
 
 	return (
@@ -28,7 +26,7 @@ const FavoriteArtists = () => {
 					className='inline-flex justify-content-between align-items-center'
 					style={{ height: '3em' }}
 				>
-					{/* <Link to='/fanhome' component={FanHome}> */}
+					{/* <Link to='/fanhome'> */}
 					<IoMdArrowBack style={{ color: '#EFEFEF' }} />
 					{/* </Link> */}
 					<span style={{ color: '#EFEFEF' }}>Favorite Artists</span>
@@ -49,7 +47,7 @@ const FavoriteArtists = () => {
 				</Container>
 			</Row>
 			<Accordion>
-				{artistData.map((artist, i) => (
+				{artistList.map((artist, i) => (
 					<Row key={`${artist}${i * 20}`}>
 						<Container fluid>
 							<Card
@@ -65,16 +63,45 @@ const FavoriteArtists = () => {
 										borderRadius: '5px'
 									}}
 								>
-									{artist.name}
+									{artist.artist_name}
 								</Accordion.Toggle>
 								<Accordion.Collapse eventKey={i + 1}>
 									<Card.Body style={{ fontSize: '0.8em' }}>
 										<Container className='justify-content-around'>
-											<Row style={{ marginBottom: '1em' }}>
+											<Container className='justify-content-center'>
+												<Image
+													src={artist.portrait_url}
+													style={{
+														width: '100%',
+														height: 'auto',
+														borderRadius: '5px'
+													}}
+												></Image>
+											</Container>
+											<Row
+												className='justify-content-center'
+												style={{
+													width: '100%',
+													height: 'auto',
+													marginLeft: '0.5em',
+													textAlign: 'center'
+												}}
+											>
+												{artist.bio}
+											</Row>
+											<Row
+												className='justify-content-center'
+												style={{ marginBottom: '1em', textAlign: 'center' }}
+											>
 												<a href={artist.website}>{artist.website}</a>
 											</Row>
-											<Row>{artist.bio}</Row>
-											<Row> Tours </Row>
+											<Row style={{ fontWeight: '700' }}> Tours </Row>
+											{artist.tour_dates.map((tour) => (
+												<Row key={`${tour.location_name}${tour.date}`}>
+													<Col>{tour.location_name}</Col>
+													<Col>{tour.date}</Col>
+												</Row>
+											))}
 										</Container>
 									</Card.Body>
 								</Accordion.Collapse>
