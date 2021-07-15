b157c9c6bdd6b8ddea43883a223223ce2661dd31
Vy Tran <vtran1022@Gmail.com>
2021-07-15 10:46:12 -0700

    working on layout

--- client/src/components/Fans/FavoriteArtists.jsx
+++ client/src/components/Fans/FavoriteArtists.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Container, Button, Accordion, Card, Image, Row } from 'react-bootstrap';
 import { IoMdArrowBack, IoMdCloseCircle } from 'react-icons/Io';
 import { VscSettings } from 'react-icons/Vsc';
+import FanHome from './FanHome.jsx';
 
 import artistData from './artist-test-data.js';
 import fanfave from './fanfave.jpeg';
@@ -10,22 +11,22 @@ import fanfave from './fanfave.jpeg';
 const FavoriteArtists = () => {
 	return (
 		<Container>
-			<nav>
-				{/* <Link to='/app' component={App}> */}
-				<IoMdArrowBack />
-				{/* </Link> */}
+			<Row className='inline-flex'>
+				<Link to='/fanhome' component={FanHome}>
+					<IoMdArrowBack />
+				</Link>
 				<VscSettings />
-			</nav>
-			<Row>
-				<div className='container-fluid'>
-					<Image src={fanfave} style={{ width: '100%', height: 'auto' }}></Image>
-				</div>
 			</Row>
 			<Row>
-				<div className='container-fluid'>
-					<Accordion>
-						{artistData.map((artist, i) => (
-							<Card key={`${artist}${i * 20}`}>
+				<Container fluid>
+					<Image src={fanfave} style={{ width: '100%', height: 'auto' }}></Image>
+				</Container>
+			</Row>
+			<Accordion>
+				{artistData.map((artist, i) => (
+					<Row key={`${artist}${i * 20}`} className='justify-content-between'>
+						<Container fluid>
+							<Card>
 								<Accordion.Toggle as={Card.Header} eventKey={i}>
 									{artist.name}
 								</Accordion.Toggle>
@@ -33,10 +34,10 @@ const FavoriteArtists = () => {
 									<Card.Body>{artist.bio}</Card.Body>
 								</Accordion.Collapse>
 							</Card>
-						))}
-					</Accordion>
-				</div>
-			</Row>
+						</Container>
+					</Row>
+				))}
+			</Accordion>
 		</Container>
 	);
 };
