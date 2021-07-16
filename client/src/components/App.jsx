61f36405940897ec9a795fabd292b50c3658c2c8
Joey Rivera <josephrivera5000@gmail.com>
2021-07-16 08:31:51 -0700

    Fixed merged conflicts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,17 @@
 import React, {useState, useEffect, useContext} from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import SplashPage from './registration-login/SplashPage.jsx'
 import Login from './registration-login/Login.jsx';
 import Register from './registration-login/Register.jsx';
 import RegistrationLogin from './registration-login/RegistrationLogin.jsx';
+import ArtistRecommendations from './Artists/ArtistRecommendations.jsx'
 import ArtistHome from './Artists/ArtistHome.jsx';
 import AppContext from './AppContext.js'
 import TestHome from './test/TestHome.jsx'
 import themes from './themes.js';
-import ArtistSearch from './Fans/ArtistSearch.jsx';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -23,23 +25,40 @@ const App = () => {
   const [website, setWebsite] = useState('www.efgsdfg.com');
   const [isArtist, setIsArtist] = useState('false');
   const serverUrl = 'serverurl';
+  const [bookings, setBookings] = useState();
   const [user_id, setUser_id] = useState(1);
   let theme = themes.neon;
 
+  // useEffect(()=> {
+  //   axios.get('/booking/view', {params:{user_id}})
+  //   .then((bookingPromise)=> {setBookings(bookingPromise.data)})
+  //   .catch((err) => console.log(err));
+
+  // }, []);
+
+  // if (!bookings) {
+  //   return <div>Loading...</div>
+  // }
+
   return (
     <>
-      <AppContext.Provider value={{username, setUsername, bio, setBio, website, setWebsite, serverUrl, isArtist, setIsArtist, theme}} >
-        <Router>
-          <Switch>
-            <Route path="/" exact component={SplashPage} />
-            <Route path="/Login" exact component={Login} />
-            <Route path="/Register" exact component={Register} />
-            <Route path="/TestHome" exact component={TestHome} />
-          </Switch>
-        </Router>
-      </AppContext.Provider>
+    <ArtistRecommendations/>
+    {/* <ArtistHome bookings={bookings}/> */}
+      {/* <AppContext.Provider value={{username, setUsername, bio, setBio, website, setWebsite, serverUrl, isArtist, setIsArtist, theme}} >
+      <Router>
+      <Switch>
+        <Route path="/" exact component={SplashPage} />
+        <Route path="/Login" exact component={Login} />
+        <Route path="/Register" exact component={Register} />
+        <Route path="/TestHome" exact component={TestHome} />
+        <Route path="/Artist">
+          <ArtistHome bookings={bookings}/>
+        </Route>
+      </Switch>
+    </Router>
+      </AppContext.Provider> */}
     </>
-  );
+  )
 };
 
 
