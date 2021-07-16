631eb46420d4cbb8aa03d53e17a7ff89b6431816
Alexander Causey <alexander.w.causey@gmail.com>
2021-07-16 14:44:04 -0700

    finished styling for fan home

--- client/src/components/Fans/FanHome.jsx
+++ client/src/components/Fans/FanHome.jsx
@@ -1,36 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, Col, Button, Image, Row } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AppContext from './../AppContext.js';
 
 const FanHome = () => {
+  const {page, form, button, selected, heading} = useContext(AppContext).theme;
 	return (
 		<>
-			<style type='text/css'>
-				{`
-        .btn-black {
-          background-color: black;
-          color: white;
-          border-color: white;
-        }
-        .btn-white {
-          background-color: white;
-          color: black;
-        }
-        .btn-xxl {
-          margin: 6rem;
-          padding: 1.5rem 1.5rem;
-          font-size: 1rem;
-          height: 8rem;
-          width: 8rem;
-        }
-        `}
-			</style>
-      <Container>
       <Image src="https://www.chicagotribune.com/resizer/VrGevjJlxmK9BUvlTMZE-ufq43A=/800x600/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/GOWRFIWXSFCETMZUKD5XWR657I.jpg" fluid />
+      <Container>
         <Row>
           <Col>
             <Link to="/Fans/FavoriteArtists">
-              <Button variant='black' size='xxl'>
+              <Button style={button} className="fanbutton">
                 Favorite Artists
               </Button>
             </Link>
@@ -39,7 +21,7 @@ const FanHome = () => {
         <Row>
           <Col>
             <Link to="/Fans/ArtistSearch">
-              <Button variant='white' size='xxl'>
+              <Button style={button} className="fanbutton">
                 Artist Search
               </Button>
             </Link>
